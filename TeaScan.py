044076171fcb5bb1d06fe4742856e3c18fc7e95a
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-05 20:44:39 +0800

    re

--- TeaScan.py
+++ TeaScan.py
@@ -13,15 +13,16 @@ list_func_ranges = get_function_ranges(pe)
 
 # delta = 0x9E3779B9 小端序
 list_tea_detal = search_data_pattern_all(pe, 'B9 79 37 9E', text_start, text_end)
+list_pair_tea_detal = []
+for index_detal in range(len(list_tea_detal)):
+    offset_tea_detal = list_tea_detal[index_detal]
+    next_offset_tea_detal = list_tea_detal[index_detal + 1 if index_detal + 1 < len(list_tea_detal) else index_detal]
+    prev_offset_tea_detal = list_tea_detal[index_detal - 1 if index_detal - 1 >= 0 else index_detal]
+    if next_offset_tea_detal - offset_tea_detal < 500 and offset_tea_detal - prev_offset_tea_detal != 0:
+        list_pair_tea_detal.append((offset_tea_detal, next_offset_tea_detal))
+    if offset_tea_detal - prev_offset_tea_detal < 500 and offset_tea_detal - next_offset_tea_detal != 0:
+        list_pair_tea_detal.append((prev_offset_tea_detal, offset_tea_detal))
 
-list_tea_detal.sort()
-paired_list = [(list_tea_detal[i], list_tea_detal[i+1]) for i in range(0, len(list_tea_detal)-1, 2)]
+for pair_tea_detal in list_pair_tea_detal:
+    print('[debug] TEA: ', hex(pair_tea_detal[0] + pe_image_base), hex(pair_tea_detal[1] + pe_image_base))
 
-max_distance = 500  # 设定一个最大允许的地址差距
-filtered_paired_list = [pair for pair in paired_list if abs(pair[1] - pair[0]) <= max_distance]
-
-# 按照地址差距从小到大排序
-filtered_paired_list.sort(key=lambda pair: abs(pair[1] - pair[0]))
-
-# 输出结果
-print(filtered_paired_list)
