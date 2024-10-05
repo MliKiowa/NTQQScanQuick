7e1f25eaf4aee0bdb22b914dedb05a7437b60266
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-05 21:08:54 +0800

    list_tea_detal_xref_func

--- TeaScan.py
+++ TeaScan.py
@@ -13,16 +13,38 @@ list_func_ranges = get_function_ranges(pe)
 
 # delta = 0x9E3779B9 小端序
 list_tea_detal = search_data_pattern_all(pe, 'B9 79 37 9E', text_start, text_end)
-list_pair_tea_detal = []
-for index_detal in range(len(list_tea_detal)):
-    offset_tea_detal = list_tea_detal[index_detal]
-    next_offset_tea_detal = list_tea_detal[index_detal + 1 if index_detal + 1 < len(list_tea_detal) else index_detal]
-    prev_offset_tea_detal = list_tea_detal[index_detal - 1 if index_detal - 1 >= 0 else index_detal]
-    if next_offset_tea_detal - offset_tea_detal < 500 and offset_tea_detal - prev_offset_tea_detal != 0:
-        list_pair_tea_detal.append((offset_tea_detal, next_offset_tea_detal))
-    if offset_tea_detal - prev_offset_tea_detal < 500 and offset_tea_detal - next_offset_tea_detal != 0:
-        list_pair_tea_detal.append((prev_offset_tea_detal, offset_tea_detal))
 
-for pair_tea_detal in list_pair_tea_detal:
-    print('[debug] TEA: ', hex(pair_tea_detal[0] + pe_image_base), hex(pair_tea_detal[1] + pe_image_base))
+# 下面对地址预处理，找到最接近的两个地址
+list_tea_detal_pair = []
+for i in range(len(list_tea_detal)):
+    min_diff = float('inf')
+    closest_value = None
+    for j in range(len(list_tea_detal)):
+        if i != j:
+            diff = abs(list_tea_detal[i] - list_tea_detal[j])
+            if diff < min_diff:
+                min_diff = diff
+                closest_value = list_tea_detal[j]
+    if min_diff <= 500:
+        list_tea_detal_pair.append((list_tea_detal[i], closest_value))
 
+# 去重
+for i in range(len(list_tea_detal_pair)):
+    for j in range(i+1, len(list_tea_detal_pair)):
+        if list_tea_detal_pair[i][0] == list_tea_detal_pair[j][1] and list_tea_detal_pair[i][1] == list_tea_detal_pair[j][0]:
+            list_tea_detal_pair[j] = (0, 0)
+
+list_tea_detal_xref_func = []
+for tea_detal_pair in list_tea_detal_pair:
+    if tea_detal_pair[1] < tea_detal_pair[0]:
+        tea_detal_pair = (tea_detal_pair[1], tea_detal_pair[0])
+    # if tea_detal_pair[0] < 0x1000000 or tea_detal_pair[1] < 0x1000000:
+    #     continue
+    tea_encrypt = find_function_containing_address(list_func_ranges, tea_detal_pair[0])
+    tea_decrypt = find_function_containing_address(list_func_ranges, tea_detal_pair[1])
+    if tea_encrypt and tea_decrypt:
+        list_tea_detal_xref_func.append((tea_encrypt['start'], tea_decrypt['start']))
+
+for tea_detal_pair in list_tea_detal_xref_func:
+    print('[result] TEA Encrypt Function: ',hex(tea_detal_pair[0]+pe_image_base))
+    print('[result] TEA Decrypt Function: ',hex(tea_detal_pair[1]+pe_image_base))
