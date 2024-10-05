b8493ab51ea6a93d4313d94add718caa0568dc72
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-05 20:01:53 +0800

    fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from capstone import *
 
 def get_function_ranges(pe):
     md = Cs(CS_ARCH_X86, CS_MODE_64)
+    md.skipdata = True
     text_section = next(s for s in pe.sections if s.Name.startswith(b'.text'))
     code = text_section.get_data()
     start_addr = text_section.VirtualAddress
@@ -13,6 +14,10 @@ def get_function_ranges(pe):
 
     for insn in md.disasm(code, start_addr):
         if insn.mnemonic == 'push' and insn.op_str == 'rbp':
+            if current_func:
+                functions.append(current_func)
+            current_func = {'start': insn.address, 'end': None}
+        elif insn.mnemonic == 'mov' and insn.op_str == 'rbp, rsp':
             if current_func:
                 functions.append(current_func)
             current_func = {'start': insn.address, 'end': None}
@@ -97,33 +102,36 @@ rdata_start, rdata_end = section_rdata_range
 
 section_text_range = get_section_range_rva(pe, '.text')
 text_start, text_end = section_text_range
+# 分析函数
+list_func_ranges = get_function_ranges(pe)
+    
 
 # 分析AES Encrypt Function
 offset_text_aes_encrypt = search_bytes(pe, rdata_start, rdata_end, b'AES_gcm_256_encrypt')
-print('[debug] Maybe AES Encrypt Text: ',hex(offset_text_aes_encrypt))
+print('[debug] Maybe AES Encrypt Text: ',hex(offset_text_aes_encrypt+pe_image_base))
 
 list_offset_aes_encrypt_xref = search_data_maybe_xref_all(pe, offset_text_aes_encrypt ,text_start,text_end)
 list_offset_aes_encrypt_func = []
 for offset_aes_encrypt in list_offset_aes_encrypt_xref:
-    rva = find_function_containing_address(get_function_ranges(pe), offset_aes_encrypt)
+    rva = find_function_containing_address(list_func_ranges, offset_aes_encrypt)
     if rva:
-        print('[debug] AES Encrypt Function: ',hex(rva['start']))
+        print('[debug] AES Encrypt Function: ',hex(rva['start']+pe_image_base))
         if rva['start'] in list_offset_aes_encrypt_func:
             continue
         list_offset_aes_encrypt_func.append(rva['start'])
-print('[result] AES Encrypt Function: ',hex(list_offset_aes_encrypt_func[len(list_offset_aes_encrypt_func)-1]))
+print('[result] AES Encrypt Function: ',hex(list_offset_aes_encrypt_func[len(list_offset_aes_encrypt_func)-1]+pe_image_base))
 
 # 分析AES Decrypt Function
 offset_text_aes_decrypt = search_bytes(pe, rdata_start, rdata_end, b'AES_gcm_256_decrypt')
-print('[debug] Maybe AES Decrypt Text: ',hex(offset_text_aes_decrypt))
+print('[debug] Maybe AES Decrypt Text: ',hex(offset_text_aes_decrypt+pe_image_base))
 
 list_offset_aes_decrypt_xref = search_data_maybe_xref_all(pe, offset_text_aes_decrypt ,text_start,text_end)
 list_offset_aes_decrypt_func = []
 for offset_aes_decrypt in list_offset_aes_decrypt_xref:
-    rva = find_function_containing_address(get_function_ranges(pe), offset_aes_decrypt)
+    rva = find_function_containing_address(list_func_ranges, offset_aes_decrypt)
     if rva:
-        print('[debug] AES Decrypt Function: ',hex(rva['start']))
+        print('[debug] AES Decrypt Function: ',hex(rva['start']+pe_image_base))
         if rva['start'] in list_offset_aes_decrypt_func:
             continue
         list_offset_aes_decrypt_func.append(rva['start'])
-print('[result] AES Decrypt Function: ',hex(list_offset_aes_decrypt_func[len(list_offset_aes_decrypt_func)-1]))
+print('[result] AES Decrypt Function: ',hex(list_offset_aes_decrypt_func[len(list_offset_aes_decrypt_func)-1]+pe_image_base))
